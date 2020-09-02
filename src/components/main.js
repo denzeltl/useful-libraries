f81372396c5f46aa7bd1865a02069350899b6e02
Denzel <denzeltlee@gmail.com>
2020-09-02 23:53:57 +0800

    Finish main styles

--- src/components/main.js
+++ src/components/main.js
@@ -1,9 +1,38 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const Main = () => {
+    const data = useStaticQuery(graphql`
+        query {
+            allLibrariesJson {
+                edges {
+                    node {
+                        title
+                        url
+                        npm
+                        slug
+                        id
+                    }
+                }
+            }
+        }
+    `);
+
     return (
         <main className="main">
-            <h1>Main</h1>
+            <ul className="list">
+                {data.allLibrariesJson.edges.map((item) => {
+                    return (
+                        <li className="item" key={item.node.id} id={`#${item.node.slug}`}>
+                            <h2>{item.node.title}</h2>
+                            <pre>{item.node.npm}</pre>
+                            <a href={item.node.url} target="_blank">
+                                Official Site
+                            </a>
+                        </li>
+                    );
+                })}
+            </ul>
         </main>
     );
 };
