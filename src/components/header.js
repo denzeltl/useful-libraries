79659b375a1ec6e625883344acf5f584cc6ee42a
Denzel <denzeltlee@gmail.com>
2020-09-04 22:38:44 +0800

    Finish media query for sp

--- src/components/header.js
+++ src/components/header.js
@@ -1,11 +1,13 @@
 import React from 'react';
 
-const Header = () => (
-    <header className="header">
-        <div className="wrapper">
-            <h1>Useful Libraries</h1>
-        </div>
-    </header>
-);
+const Header = () => {
+    return (
+        <header className="header">
+            <div className="wrapper">
+                <h1>Useful Libraries</h1>
+            </div>
+        </header>
+    );
+};
 
 export default Header;
